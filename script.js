57ad3c430705e575c0f78ed464b9fb6fbabcacee
rishunandan1234 <rishunandan89@gmail.com>
2024-01-19 23:27:10 +0530

    cs file

---
+++ script.js
@@ -0,0 +1,21 @@
+function Display(value) {
+    document.getElementById('display').value += value;
+  }
+
+  function clearDisplay() {
+    document.getElementById('display').value = '';
+  }
+
+  function backspace() {
+    let currentValue = document.getElementById('display').value;
+    document.getElementById('display').value = currentValue.slice(0, -1);
+  }
+
+  function calculate() {
+    let expression = document.getElementById('display').value;
+    try {
+      document.getElementById('display').value = eval(expression);
+    } catch (error) {
+      document.getElementById('display').value = 'Error';
+    }
+  } 
